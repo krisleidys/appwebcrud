e90c40faed9cccbea7ab36ffd1978c34480a3da6
krisleidys <118018326+krisleidys@users.noreply.github.com>
2022-11-17 15:59:37 -0500

    Add files via upload

---
+++ php/delete.php
@@ -0,0 +1,26 @@
+<?php
+require_once("conexion.php");
+$campoamodificar = $_POST['campoamodificar'];
+$nuevodato = $_POST['ndato'];
+$id = $_POST ['id'];
+print $campoamodificar;
+print "<br>";
+print $nuevodato;
+print "<br>";
+print $id;
+
+$sql=("DELETE FROM `estudiantes` WHERE ` estudiantes`.`identificacion` = id ");
+ 
+
+if (mysqli_query($conn, $sql)) {
+    echo "Exito en la insercion";
+    header("Location: ../index.html", TRUE, 301);
+    exit();
+    mysqli_close($conn);
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+
+
+?>
